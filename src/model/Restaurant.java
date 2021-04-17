3d531326b20a475da6a2347134c971c51cc30087
OpAdmiralReyes <82138827+OpAdmiralReyes@users.noreply.github.com>
2021-04-17 02:16:15 -0600

    Update Restaurant.java

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -1,5 +1,83 @@
 package model;
 
-public class Restaurant
-{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Restuarant {
+	private String name;
+	private Cuisine cuisine;
+	private Address address;
+	private List<MenuItem>menuItems;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the cuisine
+	 */
+	public Optional<Cuisine> getCuisine() {
+		return Optional.ofNullable(cuisine);
+	}
+	
+	/**
+	 * @param cuisine 
+	 */
+	public void setCuisine(Cuisine cuisine) {
+		this.cuisine = cuisine;
+	}
+	
+	/**
+	 * @return the address
+	 */
+	public Optional<Address> getAddress() {
+		return Optional.ofNullable(address);
+	}
+	
+	/**
+	 * @param address 
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	/**
+	 * @return the menuItems
+	 */
+	public List<MenuItem> getMenuItems() {
+		return menuItems;
+	}
+	
+	/**
+  *****************************************************RETURN TYPE IN UML - MenuItem - RETURN SAME ITEM SENT IN FOR CLARIFICATION TO USER?
+	 * @param menuItems
+	 */
+	public MenuItem addMenuItem(MenuItem item) {
+		menuItems.add(item);
+		return item;
+	}
+	
+	/**
+	 * ***************************************************WHAT ITEMS NEED VALIDATION 
+	 * @return the errors
+	 */
+	public List<String> isValid() {
+        List<String> errors = new ArrayList<>();
+
+        return errors;
+    }
+	
+	
+
 }
